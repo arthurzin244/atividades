99f073060d3e262fbd643f4793dc32a4a65534bd
arthurzin244 <arthurlinodovale@gmail.com>
2025-03-26 10:52:42 -0300

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main(){
+
+    float  valor , porcentagem , gorjeta, total;   
+    scanf("%f", &valor);
+    
+    scanf("%f" , &porcentagem);
+    
+    scanf("%f" , &gorjeta);
+    
+    total = ( (porcentagem/100) * valor);
+      
+
+    return 0;
+}    
+    
+    
